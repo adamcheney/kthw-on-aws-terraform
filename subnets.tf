43d5af77a0dad8f3d1d430fae4498215802a7689
Adam Cheney <adam.cheney@xero.com>
2021-01-14 10:57:18 +1300

    remove private subnets

--- subnets.tf
+++ subnets.tf
@@ -1,9 +1,9 @@
 resource "aws_subnet" "nodes_a" {
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.240.1.0/24"
   availability_zone = "${var.aws_region}a"
   tags = merge(
     map(
-      "Name", "KtHW_prv_A",
+      "Name", "KtHW_A",
       "created-by", var.owner
     ),
     var.custom_tags
@@ -11,11 +11,11 @@ resource "aws_subnet" "nodes_a" {
   vpc_id = aws_vpc.kthw.id
 }
 resource "aws_subnet" "nodes_b" {
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.240.2.0/24"
   availability_zone = "${var.aws_region}b"
   tags = merge(
     map(
-      "Name", "KtHW_prv_B",
+      "Name", "KtHW_B",
       "created-by", var.owner
     ),
     var.custom_tags
@@ -23,23 +23,11 @@ resource "aws_subnet" "nodes_b" {
   vpc_id = aws_vpc.kthw.id
 }
 resource "aws_subnet" "nodes_c" {
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.240.3.0/24"
   availability_zone = "${var.aws_region}c"
   tags = merge(
     map(
-      "Name", "KtHW_prv_C",
-      "created-by", var.owner
-    ),
-    var.custom_tags
-  )
-  vpc_id = aws_vpc.kthw.id
-}
-resource "aws_subnet" "public" {
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "${var.aws_region}c"
-  tags = merge(
-    map(
-      "Name", "KtHW_pub_C",
+      "Name", "KtHW_C",
       "created-by", var.owner
     ),
     var.custom_tags
