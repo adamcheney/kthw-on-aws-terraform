de5ddf399d73ac2d3f42b4a02281fca205f92b06
Adam Cheney <adam.cheney@xero.com>
2021-01-18 14:38:56 +1300

    add filter for alias script

--- prep-ssm-sessions.sh
+++ prep-ssm-sessions.sh
@@ -5,7 +5,7 @@ do
   for index in 0 1 2
   do
     instanceid=$(aws ec2 describe-instances --filters "Name=tag:node,Values=${role}-${index}" | \
-                 jq -j '.Reservations[].Instances[].InstanceId')
+                 jq '.Reservations[].Instances[] | select(.State.Name=="running") | .InstanceId')
     alias ${role}-${index}='aws ssm start-session --target ${instanceid}'
   done
 done
