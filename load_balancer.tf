89650dc8cf83aefc8754b685f79f355c0e8f4a07
Zachary Hing <zachary.hing@xero.com>
2021-11-11 15:50:29 +1300

    Fix load balancer targets

--- load_balancer.tf
+++ load_balancer.tf
@@ -14,7 +14,7 @@ resource "aws_lb_target_group" "kthw" {
 resource "aws_lb_target_group_attachment" "kthw" {
     count = 3
     target_group_arn = aws_lb_target_group.kthw.arn
-    target_id = element(local.worker_ip, count.index)
+    target_id = element(local.control_ip, count.index)
 }
 
 resource "aws_lb_listener" "kthw" {
